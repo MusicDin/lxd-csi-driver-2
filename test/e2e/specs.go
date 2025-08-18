f89fcb2b4eef4c134df93de48e33891b88ebde15
Din Music <din.music@canonical.com>
2025-08-18 07:56:26 +0000

    test/e2e: Add StorageClass spec
    Signed-off-by: Din Music <din.music@canonical.com>

--- test/e2e/specs.go
+++ test/e2e/specs.go
@@ -2,16 +2,106 @@ package e2e
 
 import (
 	"context"
+	"maps"
 	"time"
 
 	v1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
+	"github.com/canonical/lxd-csi-driver/internal/driver"
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
+
+// StorageClass represents a Kubernetes StorageClass.
+type StorageClass struct {
+	storagev1.StorageClass
+}
+
+// NewStorageClass creates a new StorageClass definition with the given name
+// and target LXD storage pool.
+func NewStorageClass(name string, lxdStoragePool string) *StorageClass {
+	defaultReclaimPolicy := v1.PersistentVolumeReclaimDelete
+	defaultVolumeBindingMode := storagev1.VolumeBindingWaitForFirstConsumer
+
+	manifest := storagev1.StorageClass{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: name + "-",
+		},
+		Parameters: map[string]string{
+			"storagePool": lxdStoragePool,
+		},
+		Provisioner:       driver.DefaultDriverName,
+		VolumeBindingMode: &defaultVolumeBindingMode,
+		ReclaimPolicy:     &defaultReclaimPolicy,
+	}
+
+	return &StorageClass{manifest}
+}
+
+// PrettyName returns the string consisting of StorageClass's name.
+func (sc *StorageClass) PrettyName() string {
+	return PrettyName(sc.Namespace, sc.GenerateName, sc.Name)
+}
+
+// WithParameters allows setting additional parameters for the StorageClass.
+func (sc *StorageClass) WithParameters(params map[string]string) *StorageClass {
+	if sc.Parameters == nil {
+		sc.Parameters = make(map[string]string)
+	}
+
+	maps.Copy(sc.Parameters, params)
+	return sc
+}
+
+// WithVolumeBindingMode sets the volume binding mode for the StorageClass.
+func (sc *StorageClass) WithVolumeBindingMode(mode storagev1.VolumeBindingMode) *StorageClass {
+	sc.VolumeBindingMode = &mode
+	return sc
+}
+
+// WithReclaimPolicy sets the reclaim policy for the StorageClass.
+func (sc *StorageClass) WithReclaimPolicy(mode v1.PersistentVolumeReclaimPolicy) *StorageClass {
+	sc.ReclaimPolicy = &mode
+	return sc
+}
+
+// WithDefault marks the storage class as default.
+func (sc *StorageClass) WithDefault(isDefault bool) *StorageClass {
+	if sc.Annotations == nil {
+		sc.Annotations = make(map[string]string)
+	}
+
+	key := "storageclass.kubernetes.io/is-default-class"
+	if isDefault {
+		sc.Annotations[key] = "true"
+	} else {
+		delete(sc.Annotations, key)
+	}
+
+	return sc
+}
+
+// Create creates the StorageClass in the Kubernetes cluster.
+func (sc *StorageClass) Create(ctx context.Context, client *kubernetes.Clientset) {
+	ginkgo.By("Create StorageClass " + sc.PrettyName())
+	newSC, err := client.StorageV1().StorageClasses().Create(ctx, &sc.StorageClass, metav1.CreateOptions{})
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+	if sc.Name == "" {
+		sc.Name = newSC.Name
+	}
+}
+
+// Delete deletes the StorageClass from the Kubernetes cluster.
+func (sc *StorageClass) Delete(ctx context.Context, client *kubernetes.Clientset) {
+	ginkgo.By("Delete StorageClass " + sc.PrettyName())
+	err := client.StorageV1().StorageClasses().Delete(ctx, sc.Name, metav1.DeleteOptions{})
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+}
 
 // PersistentVolumeClaim represents a Kubernetes PersistentVolumeClaim.
 type PersistentVolumeClaim struct {

canonical/lxd-csi-driver

internal/driver: Helper function to validate volume capabilities
Signed-off-by: Din Music <din.music@canonical.com>

internal/driver/capabilities.go
@@ -1,6 +1,9 @@
 package driver
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/container-storage-interface/spec/lib/go/csi"
 )
 
@@ -24,4 +27,34 @@ func NewNodeServiceCapability(cap csi.NodeServiceCapability_RPC_Type) *csi.NodeS
 			},
 		},
 	}
+}
+
+// ValidateVolumeCapabilities validates the provided volume capabilities.
+func ValidateVolumeCapabilities(volCaps ...*csi.VolumeCapability) error {
+	if len(volCaps) == 0 {
+		return fmt.Errorf("Request has no volume capabilities")
+	}
+
+	accessTypeBlock := false
+	accessTypeMount := false
+
+	for _, c := range volCaps {
+		if c.GetBlock() != nil {
+			accessTypeBlock = true
+		}
+
+		if c.GetMount() != nil {
+			accessTypeMount = true
+		}
+	}
+
+	if !accessTypeBlock && !accessTypeMount {
+		return errors.New("VolumeCapability cannot have both the mount and the block access types undefined")
+	}
+
+	if accessTypeBlock && accessTypeMount {
+		return errors.New("VolumeCapability cannot have both the mount and the block access types defined")
+	}
+
+	return nil
 }

MusicDin/lxd-csi-driver-2

internal/driver/capabilities.go
@@ -1,6 +1,8 @@
 package driver
 
 import (
+	"errors"
+
 	"github.com/container-storage-interface/spec/lib/go/csi"
 )
 
@@ -24,4 +26,34 @@ func NewNodeServiceCapability(c csi.NodeServiceCapability_RPC_Type) *csi.NodeSer
 			},
 		},
 	}
+}
+
+// ValidateVolumeCapabilities validates the provided volume capabilities.
+func ValidateVolumeCapabilities(volCaps ...*csi.VolumeCapability) error {
+	if len(volCaps) == 0 {
+		return errors.New("Request has no volume capabilities")
+	}
+
+	accessTypeBlock := false
+	accessTypeMount := false
+
+	for _, c := range volCaps {
+		if c.GetBlock() != nil {
+			accessTypeBlock = true
+		}
+
+		if c.GetMount() != nil {
+			accessTypeMount = true
+		}
+	}
+
+	if !accessTypeBlock && !accessTypeMount {
+		return errors.New("VolumeCapability cannot have both the mount and the block access types undefined")
+	}
+
+	if accessTypeBlock && accessTypeMount {
+		return errors.New("VolumeCapability cannot have both the mount and the block access types defined")
+	}
+
+	return nil
 }